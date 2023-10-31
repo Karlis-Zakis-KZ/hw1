13da6816ad888463b611c86172d805fa2d4a33bb
Karlis-Zakis-KZ <karliszakis13@gmail.com>
2023-10-31 21:38:57 +0200

    Update

---
+++ hw1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "asum.h"
+
+int main(int argc, char *argv[])
+{
+  if (argc != 2)
+  {
+    // Wrong number of arguments
+    return -1;
+  }
+
+  int n = atoi(argv[1]);
+
+  unsigned int sum = asum((unsigned int)n);
+
+  printf("%u\n", sum);
+
+  return 0;
+}
