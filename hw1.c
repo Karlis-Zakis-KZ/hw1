72e227f04cb354ebbbedd75d90ea494a630a17e5
Karlis-Zakis-KZ <karliszakis13@gmail.com>
2023-10-31 22:42:24 +0200

    test2

--- hw1.c
+++ hw1.c
@@ -2,19 +2,13 @@
 #include <stdlib.h>
 #include "asum.h"
 
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
-  if (argc != 2)
-  {
-    // Wrong number of arguments
-    return -1;
-  }
-
-  int n = atoi(argv[1]);
-
-  unsigned int sum = asum((unsigned int)n);
-
-  printf("%u\n", sum);
-
-  return 0;
+    if (argc < 2){
+        printf("Usage: %s <N>\n", argv[0]);
+        exit(1);
+    }
+    unsigned int i = atoi(argv[1]);
+    printf("%u\n", asum(i));
+    return 0;
 }
